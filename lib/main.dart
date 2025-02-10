99b2f4ee5890572aacf01adb00942917cc2274aa
asmrayhan1 <chowdhuryr828@gmail.com>
2025-02-10 22:14:52 +0600

    firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,6 +11,7 @@ import 'core/service/auth_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await Supabase.initialize(
       url: 'https://cldryweohlzpfeteqesz.supabase.co',
       anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsZHJ5d2VvaGx6cGZldGVxZXN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4NTIwMjAsImV4cCI6MjA1NDQyODAyMH0.YnAGa6qDLNUQTH2tz8cQ-gnv5vyMc1A8Xt23h7QjPic'
